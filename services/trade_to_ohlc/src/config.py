dad031d458ab32d928fffb7ad3bf5363ab366f96
Sebastian Ospina <sebasospina343@users.noreply.github.com>
2025-10-29 15:57:00 -0500

    fully dockerized version

--- services/trade_to_ohlc/src/config.py
+++ services/trade_to_ohlc/src/config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 class Config(BaseSettings):
-    kafka_input_topic: str = os.environ['KAFKA_INPUT_TOPIC']
-    kafka_output_topic: str = os.environ['KAFKA_OUTPUT_TOPIC']
+    kafka_input_topic: str = "trade"
+    kafka_output_topic: str = "ohlc"
     kafka_broker_address: str = os.environ['KAFKA_BROKER_ADDRESS']
     ohlc_windows_seconds: int = os.environ['OHLC_WINDOWS_SECONDS']
 
